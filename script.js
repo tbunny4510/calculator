ff4885a67e53a754f1ce38cca9625e7a1377addc
Sean Walters <tbunny4510@gmail.com>
2023-02-25 23:38:54 -0500

    Add functionality for operator buttons

--- script.js
+++ script.js
@@ -1,25 +1,57 @@
 const nums = document.querySelectorAll('.number');
+const ops = document.querySelectorAll('.ops');
 const screen = document.querySelector('#screen');
 const clear = document.querySelector('#clear');
 
-let total = 5;
-let arr = [];
+let total = 0;
+let displayArr = [];
+let operator;
+let init = false;
 
-let add = (n1, n2) => n1 + n2;
-let subtract = (n1, n2) => n1 - n2;
-let multiply = (n1, n2) => n1 * n2;
-let divide = (n1, n2) => n1 / n2;
+let display = (num) => {
+    displayArr.push(num);
+    screen.textContent = displayArr.join('');
+};
+
+function math(n1, op, n2) {
+    switch(op) {
+        case "+": return n1 + n2;
+            break;
+        case "-": return n1 - n2;
+            break;
+        case "*": return n1 * n2;
+            break;
+        case "/": return n1 / n2;
+    };
+};
+
+function operate(n1) {
+    total = math(total, operator, n1);
+};
 
 nums.forEach((num) => {
     num.addEventListener('click', () => {
-        arr.push(num.value);
-        screen.textContent = arr.join('');
-    })
+        display(num.value);
+    });
 });
 
-
 clear.addEventListener('click', () => {
+    init = false;
     total = 0;
-    arr = [];
+    displayArr = [];
     screen.textContent = total;
+});
+
+ops.forEach((op) => {
+    op.addEventListener('click', () => {
+        if(init === false) {
+            total = +displayArr.join('');
+            init = true;
+        } else {
+            operate(+displayArr.join(''));
+            screen.textContent = total;
+        };
+        displayArr = [];
+        operator = op.value;
+    });
 });
